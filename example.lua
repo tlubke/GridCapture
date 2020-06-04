7e46e292785a19476426da6cf62954952f6d21ec
tlubke <38451140+tlubke@users.noreply.github.com>
2020-06-03 20:50:56 -0500

    Update example.lua

--- example.lua
+++ example.lua
@@ -3,11 +3,12 @@ local gcap = require 'GridCapture/GridCapture'
 
 function init()
   gcap:set_grid(g)
+  g:all(0)
+  g:refresh()
 end
 
 function g.key(x,y,z)
   g:led(x,y,15)
-  g:all(15)
   g:refresh()
 end
 
