b320f7c1d2d1d20db4d2d0f9d162044f168cf5e0
tlubke <tyler.lubke@gmail.com>
2020-06-03 20:49:36 -0500

    themes + colors

---
+++ Themes.lua
@@ -0,0 +1,18 @@
+local Themes = {}
+
+-- table order matters
+Themes.default = {
+    "#fbfbfb", -- key
+    "#fff03e", -- led
+    "#e6e6e6", -- grid
+    "black",   -- outline
+}
+
+Themes.bw = {
+    "#000000", -- key
+    "#ffffff", -- led
+    "#000000", -- grid
+    "white",   -- outline
+}
+
+return Themes
